f592b6ebe4f47f99b24db1816b685497663c4289
jrahman-kainos <jawadur.rahman@kainos.com>
2025-08-28 14:23:45 -0300

    fixed naming and formatting.

--- src/main/resources/db/migration/V2__create_delivery_emp_table.sql
+++ src/main/resources/db/migration/V2__create_delivery_emp_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE delivery (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL,
-    bank_acc_num VARCHAR(50) NOT NULL UNIQUE,
-    nat_ins_num VARCHAR(20) NOT NULL UNIQUE
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(64) NOT NULL CHECK (name <> ""),
+    salary DECIMAL(16, 2) NOT NULL CHECK (salary >= 0),
+    bank_account_number VARCHAR(32) NOT NULL UNIQUE CHECK (bank_account_number <> ""),
+    national_insurance_number VARCHAR(32) NOT NULL UNIQUE CHECK (national_insurance_number <> ""),
 );
