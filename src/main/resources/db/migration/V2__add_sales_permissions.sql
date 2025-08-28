db7db82f895e061de9b4fb3267f7c385faad431d
Connor McLeod <connor.mcleod@kainos.com>
2025-08-28 14:47:43 -0300

    Create V2__add_sales_permissions.sql

---
+++ src/main/resources/db/migration/V2__add_sales_permissions.sql
@@ -0,0 +1,7 @@
+GRANT USAGE, SELECT ON Kainoos_Test_ConnorM.* TO "hr_legends"@"%";
+
+GRANT USAGE, SELECT ON Kainoos_Test_ConnorM.* TO "sales_legends"@"%";
+
+GRANT USAGE, SELECT ON Kainoos_Test_ConnorM.* TO "management_legends"@"%";
+
+GRANT USAGE, SELECT, INSERT, UPDATE, DELETE ON Kainoos_Test_ConnorM.sales TO 'hr_legends'@'%';
