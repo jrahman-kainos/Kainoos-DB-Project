d7d3c6a481182b2a30f9572e31a3140e82019724
jrahman-kainos <jawadur.rahman@kainos.com>
2025-08-28 15:20:29 -0300

    added sales table (#1)
    * added sales table
    
    * added checks and updated formatting
    
    * Create V2__add_sales_permissions.sql
    
    * changed permissions
    
    * swapped test database for production
    
    ---------
    
    Co-authored-by: Connor McLeod <connor.mcleod@kainos.com>

---
+++ src/main/resources/db/migration/V2__add_sales_permissions.sql
@@ -0,0 +1,5 @@
+GRANT USAGE, SELECT ON Kainoos_Prod_JawadurR.* TO "hr_legends"@"%";
+GRANT USAGE, SELECT ON Kainoos_Prod_JawadurR.* TO "sales_legends"@"%";
+GRANT USAGE, SELECT ON Kainoos_Prod_JawadurR.* TO "management_legends"@"%";
+
+GRANT USAGE, SELECT, INSERT ON Kainoos_Prod_JawadurR.sales TO 'hr_legends'@'%';
